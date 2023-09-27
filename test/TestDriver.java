d816af3e4436d70a191e35ee7367d2c7661e9195
Tahmid Chowdhury <32552650+tahmid-chowdhury@users.noreply.github.com>
2023-09-27 17:08:51 -0400

    Add files via upload

--- test/TestDriver.java
+++ test/TestDriver.java
@@ -8,14 +8,5 @@ public class TestDriver {
 
         System.out.println("The price of apples is " + apple.getPrice());
         System.out.println("The price of bananas is " + banana.getPrice());
-
-        // Different implementation
-        Apple redApple = new Apple("Red Apple", 0.99);
-        System.out.println("Name: " + redApple.getName());
-        System.out.println("Price: " + redApple.getPrice());
-
-        Banana banana = new Banana("Banana", 0.59);
-        System.out.println("Name: " + banana.getName());
-        System.out.println("Price: " + banana.getPrice());
     }
 }
