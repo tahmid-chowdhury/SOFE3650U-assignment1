7c1e8029cf5b57a9a2a57030239590e4a407382e
Tahmid Chowdhury <32552650+tahmid-chowdhury@users.noreply.github.com>
2023-09-27 18:15:34 -0400

    Update Apple.java

--- source/Apple.java
+++ source/Apple.java
@@ -3,24 +3,24 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Apple extends GroceryProduct {
-  public Apple(String productName) {
-    super(productName);
-  }
-
-  @Override
-  public void initializePriceFromDatabase(String fileName) {
-    try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-      String line;
-      while ((line = reader.readLine()) != null) {
-        String[] parts = line.split(",");
-        if (parts.length == 2 && parts[0].trim().equals(this.productName)) {
-          double newPrice = Double.parseDouble(parts[1].trim());
-          setPrice(newPrice);
-          break; // Stop reading after finding the price
-        }
-      }
-    } catch (IOException e) {
-      System.out.println("Error reading database file: " + e.getMessage());
+    public Apple(String productName) {
+        super(productName);
     }
-  }
+
+    @Override
+    public void initializePriceFromDatabase(String fileName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 2 && parts[0].trim().equals(this.productName)) {
+                    double newPrice = Double.parseDouble(parts[1].trim());
+                    setPrice(newPrice);
+                    break; // Stop reading after finding the price
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading database file: " + e.getMessage());
+        }
+    }
 }
