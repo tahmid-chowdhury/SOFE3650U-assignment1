03555bdba674f0350de4b847fd7d8c25627fb76d
Tahmid Chowdhury <32552650+tahmid-chowdhury@users.noreply.github.com>
2023-09-27 18:19:21 -0400

    Update GroceryProductFactory.java

--- source/GroceryProductFactory.java
+++ source/GroceryProductFactory.java
@@ -1,24 +1,24 @@
 public interface GroceryProductFactory {
-  public GroceryProduct createProduct(String productName);
+    public GroceryProduct createProduct(String productName);
 }
 
 class AppleFactory implements GroceryProductFactory {
-  public GroceryProduct createProduct(String productName) {
-    Apple apple = new Apple(productName);
-    return apple;
-  }
+    public GroceryProduct createProduct(String productName) {
+        Apple apple = new Apple(productName);
+        return apple;
+    }
 }
 
 class OrangeFactory implements GroceryProductFactory {
-  public GroceryProduct createProduct(String productName) {
-    Orange orange = new Orange(productName);
-    return orange;
-  }
+    public GroceryProduct createProduct(String productName) {
+        Orange orange = new Orange(productName);
+        return orange;
+    }
 }
 
 class BananaFactory implements GroceryProductFactory {
-  public GroceryProduct createProduct(String productName) {
-    Banana banana = new Banana(productName);
-    return banana;
-  }
+    public GroceryProduct createProduct(String productName) {
+        Banana banana = new Banana(productName);
+        return banana;
+    }
 }
